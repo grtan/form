e27553869579b94870db0e7c662b7c51e6dbd8aa
grtan <710598788@qq.com>
2020-04-12 22:57:28 +0800

    feat: 初始化生成器

---
+++ generator/components.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<style>
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
---
+++ generator/data.js
@@ -0,0 +1,52 @@
+export const components = {
+  string: [{
+    input: {
+      name: '单行输入',
+      icon: ''
+    },
+    textarea: {
+      name: '单行输入',
+      icon: ''
+    },
+  }],
+  number: [{
+    input: {
+      name: '单行输入',
+      icon: ''
+    },
+    textarea: {
+      name: '单行输入',
+      icon: ''
+    },
+  }],
+  boolean: [{
+    input: {
+      name: '单行输入',
+      icon: ''
+    },
+    textarea: {
+      name: '单行输入',
+      icon: ''
+    },
+  }],
+  object: [{
+    input: {
+      name: '单行输入',
+      icon: ''
+    },
+    textarea: {
+      name: '单行输入',
+      icon: ''
+    },
+  }],
+  array: [{
+    input: {
+      name: '单行输入',
+      icon: ''
+    },
+    textarea: {
+      name: '单行输入',
+      icon: ''
+    },
+  }]
+}
