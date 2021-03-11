42cbb36c5a7d9129603f8f530f54e74557abf56e
wangzhenzheng <11091506@bbktel.com>
2021-03-11 15:28:37 +0800

    feat: base组件中的input增加placeholder配置

--- src/component/base.vue
+++ src/component/base.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="fm-base__root">
     <!-- 字符串 -->
-    <template v-if="schema.type==='string'">
+    <template v-if="schema.type === 'string'">
       <!-- 颜色 -->
-      <template v-if="schema.format==='color'">
+      <template v-if="schema.format === 'color'">
         <!-- 固定枚举 -->
-        <v-enum
-          v-if="schema.enum"
-          :schema="schema"
-          :value="value"
-          @input="$listeners.input"
-        />
+        <v-enum v-if="schema.enum" :schema="schema" :value="value" @input="$listeners.input" />
         <!-- 从接口获取枚举结果，展示列表组件来选择 -->
         <!-- 普通输入 -->
         <el-color-picker
@@ -25,77 +20,40 @@
       </template>
 
       <!-- 图片、视频 -->
-      <template v-else-if="['image','video'].includes(schema.format)">
+      <template v-else-if="['image', 'video'].includes(schema.format)">
         <!-- 固定枚举 -->
-        <v-enum
-          v-if="schema.enum"
-          :schema="schema"
-          :value="value"
-          @input="$listeners.input"
-        />
+        <v-enum v-if="schema.enum" :schema="schema" :value="value" @input="$listeners.input" />
         <!-- 从接口获取枚举结果，展示列表组件来选择 -->
         <!-- 普通输入 -->
-        <v-image
-          v-else
-          :schema="schema"
-          :value="value"
-          @input="$listeners.input"
-        />
+        <v-image v-else :schema="schema" :value="value" @input="$listeners.input" />
       </template>
 
       <!-- 文件 -->
-      <template v-else-if="schema.format==='file'">
+      <template v-else-if="schema.format === 'file'">
         <!-- 固定枚举 -->
-        <v-enum
-          v-if="schema.enum"
-          :schema="schema"
-          :value="value"
-          @input="$listeners.input"
-        />
+        <v-enum v-if="schema.enum" :schema="schema" :value="value" @input="$listeners.input" />
         <!-- 从接口获取枚举结果，展示列表组件来选择 -->
         <!-- 普通输入 -->
-        <v-file
-          v-else
-          :schema="schema"
-          :value="value"
-          @input="$listeners.input"
-        />
+        <v-file v-else :schema="schema" :value="value" @input="$listeners.input" />
       </template>
 
       <!-- 日期、时间 -->
-      <template
-        v-else-if="['time','datetime','year','month','week','date'].includes(schema.format)"
-      >
+      <template v-else-if="['time', 'datetime', 'year', 'month', 'week', 'date'].includes(schema.format)">
         <!-- 固定枚举 -->
-        <v-enum
-          v-if="schema.enum"
-          :schema="schema"
-          :value="value"
-          @input="$listeners.input"
-        />
+        <v-enum v-if="schema.enum" :schema="schema" :value="value" @input="$listeners.input" />
         <!-- 从接口获取枚举结果，展示列表组件来选择 -->
         <!-- 普通输入 -->
-        <v-time
-          v-else
-          :schema="schema"
-          :value="value"
-          @input="$listeners.input"
-        />
+        <v-time v-else :schema="schema" :value="value" @input="$listeners.input" />
       </template>
 
       <!-- 普通字符串 -->
       <template v-else>
         <!-- 固定枚举 -->
-        <v-enum
-          v-if="schema.enum"
-          :schema="schema"
-          :value="value"
-          @input="$listeners.input"
-        />
+        <v-enum v-if="schema.enum" :schema="schema" :value="value" @input="$listeners.input" />
         <!-- 从接口获取枚举结果，展示列表组件来选择 -->
         <!-- 富文本 -->
         <v-richtext
-          v-else-if="schema.component==='richtext'"
+          v-else-if="schema.component === 'richtext'"
           :schema="schema"
           :value="value"
           @input="$listeners.input"
@@ -104,90 +62,60 @@
         <el-input
           v-else
           :value="value"
-          :type="schema.component==='textarea'?'textarea':undefined"
-          :minlength="typeof schema.minLength==='number'?schema.minLength:undefined"
-          :maxlength="typeof schema.maxLength==='number'?schema.maxLength:undefined"
+          :type="schema.component === 'textarea' ? 'textarea' : undefined"
+          :minlength="typeof schema.minLength === 'number' ? schema.minLength : undefined"
+          :maxlength="typeof schema.maxLength === 'number' ? schema.maxLength : undefined"
           :readonly="schema.readonly"
           :clearable="true"
-          placeholder="请输入内容"
+          :placeholder="schema.placeholder || '请输入内容'"
           @input="$listeners.input"
         />
       </template>
     </template>
 
     <!-- 数字 -->
-    <template v-else-if="schema.type==='number'">
+    <template v-else-if="schema.type === 'number'">
       <!-- 固定枚举 -->
-      <v-enum
-        v-if="schema.enum"
-        :schema="schema"
-        :value="value"
-        @input="$listeners.input"
-      />
+      <v-enum v-if="schema.enum" :schema="schema" :value="value" @input="$listeners.input" />
       <!-- 从接口获取枚举结果 -->
       <!-- 普通输入 -->
       <el-input-number
         v-else
         :value="value"
-        :min="typeof schema.minimum==='number'?schema.minimum:undefined"
-        :max="typeof schema.maximum==='number'?schema.maximum:undefined"
-        :step="typeof schema.multipleOf==='number'?schema.multipleOf:undefined"
-        :step-strictly="typeof schema.multipleOf==='number'"
+        :min="typeof schema.minimum === 'number' ? schema.minimum : undefined"
+        :max="typeof schema.maximum === 'number' ? schema.maximum : undefined"
+        :step="typeof schema.multipleOf === 'number' ? schema.multipleOf : undefined"
+        :step-strictly="typeof schema.multipleOf === 'number'"
         :disabled="schema.readonly"
         @input="$listeners.input"
       />
     </template>
 
     <!-- 布尔值 -->
-    <template v-else-if="schema.type==='boolean'">
+    <template v-else-if="schema.type === 'boolean'">
       <el-switch
-        v-if="schema.component==='switch'"
+        v-if="schema.component === 'switch'"
         :value="value"
         :disabled="schema.readonly"
         @input="$listeners.input"
       />
-      <el-checkbox
-        v-else
-        :disabled="schema.readonly"
-        :value="value"
-        @input="$listeners.input"
-      />
+      <el-checkbox v-else :disabled="schema.readonly" :value="value" @input="$listeners.input" />
     </template>
 
     <!-- 地址 -->
-    <template v-else-if="schema.type==='address'">
+    <template v-else-if="schema.type === 'address'">
       <!-- 固定枚举 -->
-      <v-enum
-        v-if="schema.enum"
-        :schema="schema"
-        :value="value"
-        @input="$listeners.input"
-      />
+      <v-enum v-if="schema.enum" :schema="schema" :value="value" @input="$listeners.input" />
       <!-- 普通输入 -->
-      <v-address
-        v-else
-        :schema="schema"
-        :value="value"
-        @input="$listeners.input"
-      />
+      <v-address v-else :schema="schema" :value="value" @input="$listeners.input" />
     </template>
 
     <!-- 日期、时间范围 -->
-    <template v-else-if="schema.type==='range'">
+    <template v-else-if="schema.type === 'range'">
       <!-- 固定枚举 -->
-      <v-enum
-        v-if="schema.enum"
-        :schema="schema"
-        :value="value"
-        @input="$listeners.input"
-      />
+      <v-enum v-if="schema.enum" :schema="schema" :value="value" @input="$listeners.input" />
       <!-- 普通输入 -->
-      <v-time
-        v-else
-        :schema="schema"
-        :value="value"
-        @input="$listeners.input"
-      />
+      <v-time v-else :schema="schema" :value="value" @input="$listeners.input" />
     </template>
   </div>
 </template>
@@ -202,7 +130,7 @@ import VRichtext from './richtext/index'
 
 export default {
   components: {
-    VEnum (resolve) {
+    VEnum(resolve) {
       resolve(VEnum)
     },
     VImage,
