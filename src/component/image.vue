ae53c10ff088f94385d5b480ec913a6bd2880b81
谭新 <xin.tan@vivo.com>
2021-03-12 14:52:09 +0800

    feat: 给image、file组件添加onProgress支持；修改文档

--- src/component/image.vue
+++ src/component/image.vue
@@ -1,8 +1,8 @@
 <template>
-  <div v-if="value||!schema.readonly" :class="['fm-image__root',{'fm-image--uploaded':schema.readonly||value}]">
+  <div v-if="value || !schema.readonly" :class="['fm-image__root', { 'fm-image--uploaded': schema.readonly || value }]">
     <el-upload
       ref="upload"
-      :action="schema.action||''"
+      :action="schema.action || ''"
       :name="schema.name"
       :headers="schema.headers"
       :with-credentials="schema.withCredentials"
@@ -11,11 +11,12 @@
       list-type="picture-card"
       :show-file-list="false"
       :before-upload="onBeforeUpload"
+      :on-progress="onProgress"
       :on-success="onSuccess"
     >
-      <i v-if="!schema.readonly&&!value" class="el-icon-plus" />
+      <i v-if="!schema.readonly && !value" class="el-icon-plus" />
       <div v-else ref="box" class="fm-image__box">
-        <img v-if="schema.format==='image'" :src="value">
+        <img v-if="schema.format === 'image'" :src="value" />
         <video v-else :src="value">您的浏览器不支持 video 标签</video>
         <el-row type="flex" justify="center" @click.native.stop>
           <el-col :span="6">
@@ -31,11 +32,76 @@
       </div>
     </el-upload>
     <el-dialog class="fm-image__preview" :visible.sync="preview" append-to-body>
-      <img v-if="schema.format==='image'" width="100%" :src="value">
+      <img v-if="schema.format === 'image'" width="100%" :src="value" />
       <video v-else :src="value" controls>您的浏览器不支持 video 标签</video>
     </el-dialog>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    schema: {
+      type: Object,
+      required: true
+    },
+    value: {
+      type: String,
+      default: undefined
+    }
+  },
+  data() {
+    return {
+      preview: false // 是否显示预览界面
+    }
+  },
+  methods: {
+    onValidateFail(reason) {
+      reason && this.$emit('input', `validate:${reason}`)
+    },
+    onBeforeUpload(file) {
+      if (typeof this.schema.fileValidator !== 'function') {
+        return
+      }
+
+      // 检验失败后不会上传文件
+      return this.schema.fileValidator(file, this.onValidateFail)
+    },
+    onProgress() {
+      if (typeof this.schema.onProgress !== 'function') {
+        return
+      }
+
+      return this.schema.onProgress(...arguments)
+    },
+    onSuccess(res, file) {
+      let url
+
+      if (typeof this.schema.urlFetcher === 'string') {
+        const response = file.response
+        // eslint-disable-next-line no-new-func
+        const fn = new Function('response', `return (${this.schema.urlFetcher})`)
+
+        url = fn(response)
+      } else {
+        url = this.schema.urlFetcher(file.response)
+      }
+
+      this.$refs.upload.clearFiles()
+      this.$emit('input', url)
+    },
+    onPreview() {
+      this.preview = true
+    },
+    onEdit() {
+      this.$refs.box.click()
+    },
+    onDelete() {
+      this.$emit('input', undefined)
+    }
+  }
+}
+</script>
 
 <style lang="less">
 .fm-image {
@@ -101,61 +167,3 @@
   }
 }
 </style>
-
-<script>
-export default {
-  props: {
-    schema: {
-      type: Object,
-      required: true
-    },
-    value: {
-      type: String,
-      default: undefined
-    }
-  },
-  data () {
-    return {
-      preview: false // 是否显示预览界面
-    }
-  },
-  methods: {
-    onValidateFail (reason) {
-      reason && this.$emit('input', `validate:${reason}`)
-    },
-    onBeforeUpload (file) {
-      if (typeof this.schema.fileValidator !== 'function') {
-        return
-      }
-
-      // 检验失败后不会上传文件
-      return this.schema.fileValidator(file, this.onValidateFail)
-    },
-    onSuccess (res, file) {
-      let url
-
-      if (typeof this.schema.urlFetcher === 'string') {
-        const response = file.response
-        // eslint-disable-next-line no-new-func
-        const fn = new Function('response', `return (${this.schema.urlFetcher})`)
-
-        url = fn(response)
-      } else {
-        url = this.schema.urlFetcher(file.response)
-      }
-
-      this.$refs.upload.clearFiles()
-      this.$emit('input', url)
-    },
-    onPreview () {
-      this.preview = true
-    },
-    onEdit () {
-      this.$refs.box.click()
-    },
-    onDelete () {
-      this.$emit('input', undefined)
-    }
-  }
-}
-</script>
